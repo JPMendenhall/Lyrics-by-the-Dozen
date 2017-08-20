93c9fc0b45fc6a9bdca9c82e7f5ab38ffe7bd387
James Mendenhall <Nattysoccer9@yahoo.com>
2017-08-20 10:06:47 -0600

    Fixed redirect error, fixed stuck page on misspelled artist, and added new track button

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $('.guessTrack').hide()
 $('.fail').hide()
 $('.success').hide()
 $('.twelveMore').hide()
-
+$('.songLink2').hide()
 
 
 //Input band name
@@ -21,30 +21,32 @@ $(".buttonOne").click(function(event) {
       $('.buttonOne').hide();
       $('#bandInput').hide();
       $('.artist').append(" " + artist);
-      if (bandInput == "") {
-        alert("Oops!  Please enter a valid band name.")
+      if ((bandInput == "") || (bandInput == " ") || (bandInput == "  ") || (bandInput == "   ") || (bandInput == "    ") || (bandInput == "     ") || (bandInput == "      ")) {
+        alert("Oops!  Please enter a valid band name.");
+        location.reload();
       }
       $.get("https://galvanize-cors-proxy.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + lyricID + "&apikey=c9cf420cde3c6f076d884df06df9c0cc")
         .then(function(data) {
           var obj = JSON.parse(data);
+          if (obj.message.header.status_code == "404") {
+            alert("Artist not found! \n\n Please check the spelling of the artist and try again.  \n\n If the spelling is correct, the selected artist does not currently have lyrics available in the MusixMatch lyrics database.");
+            location.reload();
+          }
           var lyricsReturn = obj.message.body.lyrics["lyrics_body"];
-          console.log("hello 1")
-          if (lyricsReturn.length == 0) {
-            console.log("hello 2")
+          if (lyricsReturn.length < 10) {
             alert("No lyrics available! Possible reasons include \n \n 1) The track retrieved contains no lyrics (instrumental)  \n 2) We do not have permission to to display lyrics by this artist \n \n Please either resubmit your artist or try a new artist");
             $('.buttonOne').show();
             $('#bandInput').show();
-            console.log("hello 3")
-            console.log(location.reload)
             location.reload();
           } else if (lyricsReturn.length >= 5) {
             $('.guessTrack').show();
-            $('.twelveMore').show()
+            $('.twelveMore').show();
+            $('.buttonNewTrack').show()
           }
           twelveWords = lyricsReturn.split(/\s+/).slice(0, 12).join(" ");
           twelveMoreWords = lyricsReturn.split(/\s+/).slice(12, 24).join(" ");
           trackName = (trackList[i].track.track_name);
-          lyricsResults = $('.populate').html(twelveWords)
+          lyricsResults = $('.populate').html(twelveWords);
           $(".songLink").click(function() {
             window.open("" + songLink + "".href, '_blank');
             win.focus();
@@ -60,6 +62,48 @@ $(".twelveMore").click(function() {
   $(this).hide();
 })
 
+//New Track buttonTwo
+
+$(".buttonNewTrack").click(function(event) {
+  event.preventDefault()
+  bandInput = decodeURIComponent($("#bandInput").val());
+  $.get("https://galvanize-cors-proxy.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_artist=" + bandInput + "&apikey=c9cf420cde3c6f076d884df06df9c0cc")
+    .then(function(data) {
+      var obj = JSON.parse(data);
+      var trackList = obj.message.body.track_list;
+      i = Math.floor(Math.random() * trackList.length);
+      var lyricID = trackList[i].track.track_id;
+      var songLink2 = trackList[i].track.track_share_url;
+      $('.songLink2').show();
+      $('.songLink').hide();
+      $('.buttonNewTrack').hide();
+      var artist = trackList[i].track.artist_name;
+      $.get("https://galvanize-cors-proxy.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + lyricID + "&apikey=c9cf420cde3c6f076d884df06df9c0cc")
+        .then(function(data) {
+          var obj = JSON.parse(data);
+          var lyricsReturn = obj.message.body.lyrics["lyrics_body"];
+          if (lyricsReturn.length < 10) {
+            alert("No lyrics available! Possible reasons include \n \n 1) The track retrieved contains no lyrics (instrumental)  \n 2) We do not have permission to to display lyrics by this artist \n \n Please either resubmit your artist or try a new artist");
+            $('#bandInput').show();
+            location.reload();
+          } else if (lyricsReturn.length >= 5) {
+            $('.guessTrack').show();
+            $('.twelveMore').show();
+            $('.buttonTwo').show()
+          }
+          twelveWords = lyricsReturn.split(/\s+/).slice(0, 12).join(" ");
+          twelveMoreWords = lyricsReturn.split(/\s+/).slice(12, 24).join(" ");
+          trackName = (trackList[i].track.track_name);
+          lyricsResults = $('.populate').html(twelveWords)
+          $(".songLink2").click(function() {
+            window.open("" + songLink2 + "".href, '_blank');
+            win.focus();
+          })
+        })
+    })
+})
+
+
 // Guess song title
 
 $(".buttonTwo").click(function(event) {
@@ -74,7 +118,6 @@ $(".buttonTwo").click(function(event) {
       var guess = $('#trackGuess').val();
       var guessed = guess.toLowerCase();
       var songLink = trackList[i].track.track_share_url;
-      console.log(songLink);
       if (trackName.includes(guessed) && guessed.length > 2) {
         $('.success').show();
         $('.subReset').hide();
@@ -84,7 +127,6 @@ $(".buttonTwo").click(function(event) {
             scrollTop: $(".successGif").offset().top
           },
           'slow');
-
       } else if (guessed == trackName) {
         $('.success').show();
         $('.subReset').hide();
@@ -94,8 +136,6 @@ $(".buttonTwo").click(function(event) {
             scrollTop: $(".successGif").offset().top
           },
           'slow');
-
-
       } else if (guessed !== trackName && guessed.length >= 1) {
         $('.fail').show();
         $('.failHide').hide();
@@ -104,14 +144,8 @@ $(".buttonTwo").click(function(event) {
             scrollTop: $(".failGif").offset().top
           },
           'slow');
-
       }
     })
-})
-
-$(".songLink").click(function() {
-  window.open("" + songLink + "".href, '_blank');
-  win.focus();
 })
 
 $(".reload").click(function(event) {
@@ -123,41 +157,3 @@ $(".buttonTwo").click(function(event) {
   event.preventDefault();
   $('.giveUp').hide();
 })
-
-
-
-// $(document).ready(function() {
-//   $('.button').hide(0); //or do it through css
-//   $('a.tab1').click(function() {
-//     $('.button').show();
-//
-//   });
-//
-//   //otherTab is the class for the tabs other than tab1
-//   $('a.otherTab').click(function() {
-//     $('.button').hide();
-//   });
-
-// });
-
-//   .then(function(data) {
-//     for (var i = 0; i < data.length; i++) {
-//       $('.drop-down').append(`<option value="${data[i].title}">${data[i].title}</option>`);
-//       console.log(data)
-//     }
-//     $("select").on("change", function(event) {
-//       console.log(event.target.value)
-//       console.log(data)
-//       for (var i = 0; i < data.length; i++) {
-//         console.log(data[i])
-//         if (event.target.value == data[i].title) {
-//           $('.role-preview').attr("src", data[i].img)
-//         }
-//       }
-//     })
-//   })
-// $('.role-preview').attr("src", data[i].img)
-// }
-// }
-// })
-// })
